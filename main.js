4ffa8afbf5863c98edd00bc078b41c7d4687c19d
fatxullayevbobur82-arch <fatxullayevbobur82@gmail.com>
2025-11-09 19:01:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// console.log(12,16);
+
+// console.log(25);
+// console.log("Murod");
+
+var ism = prompt("ismingizni kiriting");
+var familya = prompt("familangizi kiritin");
+var yosh = +prompt("yoshingizni kiring");
+
+console.log(ism);
+console.log(familya);
+console.log(yosh);
+
+
+
